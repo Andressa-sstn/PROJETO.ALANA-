80fc685e11dbcb7507902d90b56dd173363df452
andressa silva santos <dressasillva5@gmail.com>
2025-02-26 22:21:49 -0300

    Create inicializacao.h

---
+++ inicializacao.h
@@ -0,0 +1,12 @@
+#ifndef INICIALIZACAO_H
+#define INICIALIZACAO_H
+
+#include <stdint.h>
+
+// Função para inicializar os sensores
+void inicializar_sensores(void);
+
+// Função para inicializar a comunicação
+void inicializar_comunicacao(void);
+
+#endif  // INICIALIZACAO_H
