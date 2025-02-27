49f030edcf5c81784cd8e01e8589907fa331acc3
andressa silva santos <dressasillva5@gmail.com>
2025-02-26 22:22:13 -0300

    Create  inicializacao.c

---
+++ inicializacao.c
@@ -0,0 +1,15 @@
+#include "inicializacao.h"
+#include "sensor.h"
+#include "comunicacao.h"
+
+// Função para inicializar os sensores
+void inicializar_sensores(void) {
+    // Inicialização dos sensores
+    // Exemplo: configurar os pinos como entradas
+}
+
+// Função para inicializar a comunicação
+void inicializar_comunicacao(void) {
+    // Inicialização da comunicação
+    // Exemplo: configurar os pinos como saídas
+}
