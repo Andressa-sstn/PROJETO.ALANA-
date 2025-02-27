46aadae05cb3016af38cd25c487ce277420aa0d9
andressa silva santos <dressasillva5@gmail.com>
2025-02-26 22:23:06 -0300

    Create exibir_resultados.c

---
+++ exibir_resultados.c
@@ -0,0 +1,9 @@
+#include "exibir_resultados.h"
+#include "sensor.h"
+#include "comunicacao.h"
+
+// Função para exibir os resultados
+void exibir_resultados(float temperatura, float ultrassom_dados, bool alerta) {
+    // Exibição dos resultados
+    // Exemplo: usar a biblioteca OLED para exibir os dados
+}
