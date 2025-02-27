6453785f1e6050e3d3b755b152fac3bdbc8834f5
andressa silva santos <dressasillva5@gmail.com>
2025-02-26 22:22:43 -0300

    Create exibir_resultados.h

---
+++ exibir_resultados.h
@@ -0,0 +1,9 @@
+#ifndef EXIBIR_RESULTADOS_H
+#define EXIBIR_RESULTADOS_H
+
+#include <stdint.h>
+
+// Função para exibir os resultados
+void exibir_resultados(float temperatura, float ultrassom_dados, bool alerta);
+
+#endif  // EXIBIR_RESULTADOS_H
